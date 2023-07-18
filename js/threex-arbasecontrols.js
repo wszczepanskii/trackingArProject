34f02d207fce31ba69c40025bb66126711a9e5e0
bvmbo <wiktorsz2001@wp.pl>
2023-07-18 15:56:38 +0200

    added import module

--- js/threex-arbasecontrols.js
+++ js/threex-arbasecontrols.js
@@ -1,21 +1,23 @@
-var THREEx = THREEx || {}
+import * as THREE from "three";
 
-THREEx.ArBaseControls = function(object3d){
-	this.id = THREEx.ArBaseControls.id++
+var THREEx = THREEx || {};
 
-	this.object3d = object3d
+THREEx.ArBaseControls = function (object3d) {
+	this.id = THREEx.ArBaseControls.id++;
+
+	this.object3d = object3d;
 	this.object3d.matrixAutoUpdate = false;
-	this.object3d.visible = false
+	this.object3d.visible = false;
 
 	// Events to honor
 	// this.dispatchEvent({ type: 'becameVisible' })
 	// this.dispatchEvent({ type: 'markerVisible' })	// replace markerFound
 	// this.dispatchEvent({ type: 'becameUnVisible' })
-}
+};
 
-THREEx.ArBaseControls.id = 0
+THREEx.ArBaseControls.id = 0;
 
-Object.assign( THREEx.ArBaseControls.prototype, THREE.EventDispatcher.prototype );
+Object.assign(THREEx.ArBaseControls.prototype, THREE.EventDispatcher.prototype);
 
 //////////////////////////////////////////////////////////////////////////////
 //		Functions
@@ -23,14 +25,14 @@ Object.assign( THREEx.ArBaseControls.prototype, THREE.EventDispatcher.prototype 
 /**
  * error catching function for update()
  */
-THREEx.ArBaseControls.prototype.update = function(){
-	console.assert(false, 'you need to implement your own update')
-}
+THREEx.ArBaseControls.prototype.update = function () {
+	console.assert(false, "you need to implement your own update");
+};
 
 /**
  * error catching function for name()
  */
-THREEx.ArBaseControls.prototype.name = function(){
-	console.assert(false, 'you need to implement your own .name()')
-	return 'Not yet implemented - name()'
-}
+THREEx.ArBaseControls.prototype.name = function () {
+	console.assert(false, "you need to implement your own .name()");
+	return "Not yet implemented - name()";
+};
