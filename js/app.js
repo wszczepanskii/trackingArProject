54d62e9a74a94655e4223c647a30fae15b8cff5a
bvmbo <wiktorsz2001@wp.pl>
2023-07-19 10:59:12 +0200

    added tracking

--- js/app.js
+++ js/app.js
@@ -1,94 +1,212 @@
-import * as THREE from "three";
+// import * as THREE from "three.js";
 // import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 // import { ARButton } from "three/addons/webxr/ARButton.js";
 // import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 // import { RGBELoader } from "three/addons/loaders/lwo/RGBELoader.js";
 
-let camera, scene, renderer, controller, controls, reticle;
-let ArToolkitSource, ArToolkitContext, ArMarkerControls;
+// let camera, scene, renderer, controller, controls, reticle;
+// let ArToolkitSource, ArToolkitContext, ArMarkerControls;
 
-const init = () => {
-	const container = document.createElement("div");
-	document.body.append(container);
+// const init = () => {
+// 	const container = document.createElement("div");
+// 	document.body.append(container);
 
+// 	scene = new THREE.Scene();
+
+// 	camera = new THREE.Camera();
+// 	scene.add(camera);
+
+// 	renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+// 	renderer.setPixelRatio(window.devicePixelRatio);
+// 	renderer.setSize(window.innerWidth, window.innerHeight);
+// 	renderer.useLegacyLights = false;
+// 	renderer.xr.enabled = true;
+// 	document.body.append(renderer.domElement);
+
+// 	const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+// 	light.position.set(0.5, 1, 0.25);
+// 	scene.add(light);
+
+// 	window.addEventListener("resize", onWindowResize, false);
+
+// 	ArToolkitSource = new THREEx.ArToolkitSource({
+// 		sourceType: "webcam",
+// 	});
+
+// 	ArToolkitSource.init(() => {
+// 		setTimeout(() => {
+// 			ArToolkitSource.onResizeElement();
+// 			ArToolkitSource.copyElementSizeTo(render.domElement);
+// 		}, 2000);
+// 	});
+
+// 	ArToolkitContext = new THREEx.ArToolkitContext({
+// 		cameraParametersUrl: "camera_para.dat",
+// 		detectionMode: "mono",
+// 	});
+
+// 	ArToolkitContext.init(() => {
+// 		camera.projectionMatrix.copy(ArToolkitContext.getProjectionMatrix());
+// 	});
+
+// 	ArMarkerControls = new THREEx.ArMarkerControls(ArToolkitContext, camera, {
+// 		type: "pattern",
+// 		patternUrl: "./markers/hiro.patt",
+// 		changeMatrixMode: "cameraTransformMatrix",
+// 	});
+
+// 	scene.visible = false;
+
+// 	const geometry = new THREE.BoxGeometry(1, 1, 1);
+// 	const material = new THREE.MeshNormalMaterial({
+// 		transparent: true,
+// 		opacity: 0.5,
+// 		side: THREE.DoubleSide,
+// 	});
+// 	const cube = new THREE.Mesh(geometry, material);
+// 	cube.position.y = 0.5;
+
+// 	scene.add(cube);
+// };
+
+// const onWindowResize = () => {
+// 	camera.aspect = window.innerWidth / window.innerHeight;
+// 	camera.updateProjectionMatrix();
+
+// 	renderer.setSize(window.innerWidth, window.innerHeight);
+// };
+
+// const animate = () => {
+// 	renderer.setAnimationLoop(render);
+// 	ArToolkitContext.update(ArToolkitSource.domElement);
+// 	scene.visible = camera.visible;
+// };
+
+// const render = () => {
+// 	renderer.render(scene, camera);
+// };
+
+// init();
+// animate();
+
+var scene, camera, renderer, clock, deltaTime, totalTime;
+
+var arToolkitSource, arToolkitContext;
+
+var markerRoot1, markerRoot2;
+
+var mesh1;
+
+initialize();
+animate();
+
+function initialize() {
 	scene = new THREE.Scene();
 
-	camera = new THREE.PerspectiveCamera(
-		70,
-		window.innerWidth / window.innerHeight,
-		0.01,
-		40
-	);
+	let ambientLight = new THREE.AmbientLight(0xcccccc, 0.5);
+	scene.add(ambientLight);
 
-	renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-	renderer.setPixelRatio(window.devicePixelRatio);
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.useLegacyLights = false;
-	renderer.xr.enabled = true;
-	document.body.append(renderer.domElement);
+	camera = new THREE.Camera();
+	scene.add(camera);
 
-	const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
-	light.position.set(0.5, 1, 0.25);
-	scene.add(light);
+	renderer = new THREE.WebGLRenderer({
+		antialias: true,
+		alpha: true,
+	});
+	renderer.setClearColor(new THREE.Color("lightgrey"), 0);
+	renderer.setSize(640, 480);
+	renderer.domElement.style.position = "absolute";
+	renderer.domElement.style.top = "0px";
+	renderer.domElement.style.left = "0px";
+	document.body.appendChild(renderer.domElement);
 
-	window.addEventListener("resize", onWindowResize, false);
+	clock = new THREE.Clock();
+	deltaTime = 0;
+	totalTime = 0;
 
-	ArToolkitSource = new THREEx.ArToolkitSource({
+	////////////////////////////////////////////////////////////
+	// setup arToolkitSource
+	////////////////////////////////////////////////////////////
+
+	arToolkitSource = new THREEx.ArToolkitSource({
 		sourceType: "webcam",
 	});
 
-	ArToolkitSource.init(() => {
-		setTimeout(() => {
-			ArToolkitSource.onResizeElement();
-			ArToolkitSource.copyElementSizeTo(render.domElement);
-		}, 2000);
+	function onResize() {
+		arToolkitSource.onResize();
+		arToolkitSource.copySizeTo(renderer.domElement);
+		if (arToolkitContext.arController !== null) {
+			arToolkitSource.copySizeTo(arToolkitContext.arController.canvas);
+		}
+	}
+
+	arToolkitSource.init(function onReady() {
+		onResize();
 	});
 
-	ArToolkitContext = new THREEx.ArToolkitContext({
-		cameraParametersUrl: "camera_para.dat",
+	// handle resize event
+	window.addEventListener("resize", function () {
+		onResize();
+	});
+
+	////////////////////////////////////////////////////////////
+	// setup arToolkitContext
+	////////////////////////////////////////////////////////////
+
+	// create atToolkitContext
+	arToolkitContext = new THREEx.ArToolkitContext({
+		cameraParametersUrl: "js/camera_para.dat",
 		detectionMode: "mono",
 	});
 
-	ArToolkitContext.init(() => {
-		camera.projectionMatrix.copy(ArToolkitContext.getProjectionMatrix());
+	// copy projection matrix to camera when initialization complete
+	arToolkitContext.init(function onCompleted() {
+		camera.projectionMatrix.copy(arToolkitContext.getProjectionMatrix());
 	});
 
-	ArMarkerControls = new THREEx.ArMarkerControls(ArToolkitContext, camera, {
-		type: "pattern",
-		patternUrl: "./markers/hiro.patt",
-		changeMatrixMode: "cameraTransformMatrix",
-	});
+	////////////////////////////////////////////////////////////
+	// setup markerRoots
+	////////////////////////////////////////////////////////////
 
-	scene.visible = false;
+	// build markerControls
+	markerRoot1 = new THREE.Group();
+	scene.add(markerRoot1);
+	let markerControls1 = new THREEx.ArMarkerControls(
+		arToolkitContext,
+		markerRoot1,
+		{
+			type: "pattern",
+			patternUrl: "markers/hiro.patt",
+		}
+	);
 
-	const geometry = new THREE.CubeGeometry(1, 1, 1);
-	const material = new THREE.MeshNormalMaterial({
+	let geometry1 = new THREE.CubeGeometry(1, 1, 1);
+	let material1 = new THREE.MeshNormalMaterial({
 		transparent: true,
 		opacity: 0.5,
 		side: THREE.DoubleSide,
 	});
-	const cube = new THREE.Mesh(geometry, material);
-	cube.position.y = 0.5;
 
-	scene.add(cube);
-};
+	mesh1 = new THREE.Mesh(geometry1, material1);
+	mesh1.position.y = 0.5;
 
-const onWindowResize = () => {
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
+	markerRoot1.add(mesh1);
+}
 
-	renderer.setSize(window.innerWidth, window.innerHeight);
-};
+function update() {
+	// update artoolkit on every frame
+	if (arToolkitSource.ready !== false)
+		arToolkitContext.update(arToolkitSource.domElement);
+}
 
-const animate = () => {
-	renderer.setAnimationLoop(render);
-	ArToolkitContext.update(ArToolkitSource.domElement);
-	scene.visible = camera.visible;
-};
-
-const render = () => {
+function render() {
 	renderer.render(scene, camera);
-};
+}
 
-init();
-animate();
+function animate() {
+	requestAnimationFrame(animate);
+	deltaTime = clock.getDelta();
+	totalTime += deltaTime;
+	update();
+	render();
+}
