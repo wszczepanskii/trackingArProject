bb812302ecf839e110f608715febf95e8f36b501
bvmbo <wiktorsz2001@wp.pl>
2023-07-21 17:06:38 +0200

    renderer styles

--- js/app.js
+++ js/app.js
@@ -30,10 +30,10 @@ function initialize() {
 		antialias: true,
 		alpha: true,
 	});
-	// renderer.setClearColor(new THREE.Color("lightgrey"), 0);
-	// renderer.domElement.style.position = "absolute";
-	// renderer.domElement.style.top = "0px";
-	// renderer.domElement.style.left = "0px";
+	renderer.setClearColor(new THREE.Color("lightgrey"), 0);
+	renderer.domElement.style.position = "absolute";
+	renderer.domElement.style.top = "0px";
+	renderer.domElement.style.left = "0px";
 	document.body.appendChild(renderer.domElement);
 
 	clock = new THREE.Clock();
