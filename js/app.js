63696b76895b5156b8ac551d8563826242a61692
bvmbo <wiktorsz2001@wp.pl>
2023-07-21 17:00:21 +0200

    added changeMatrixMode

--- js/app.js
+++ js/app.js
@@ -28,11 +28,10 @@ function initialize() {
 		antialias: true,
 		alpha: true,
 	});
-	renderer.setClearColor(new THREE.Color("lightgrey"), 0);
-	renderer.setSize(640, 480);
-	renderer.domElement.style.position = "absolute";
-	renderer.domElement.style.top = "0px";
-	renderer.domElement.style.left = "0px";
+	// renderer.setClearColor(new THREE.Color("lightgrey"), 0);
+	// renderer.domElement.style.position = "absolute";
+	// renderer.domElement.style.top = "0px";
+	// renderer.domElement.style.left = "0px";
 	document.body.appendChild(renderer.domElement);
 
 	clock = new THREE.Clock();
@@ -92,6 +91,7 @@ function initialize() {
 		{
 			type: "pattern",
 			patternUrl: "markers/hiro.patt",
+			changeMatrixMode: "cameraTransformMatrix",
 			// smoothCount: "10",
 		}
 	);
