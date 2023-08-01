f381cde088cf931119f46d6ec6143896e941d357
bvmbo <wiktorsz2001@wp.pl>
2023-08-01 19:01:01 +0200

    working old version no full screen:

--- js/app.js
+++ js/app.js
@@ -1,25 +1,21 @@
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
-var scene, camera, renderer, clock, deltaTime, totalTime;
+let scene, camera, renderer, clock, deltaTime, totalTime;
 
-var arToolkitSource, arToolkitContext;
+let arToolkitSource, arToolkitContext;
 
-var markerRoot1, markerRoot2;
+let markerRoot1, markerRoot2;
 
 const animationBtn = document.querySelector(".play-animation");
 
-var mesh1,
-	glbScene,
+let glbScene,
 	glbModel,
 	glbAnimations,
 	mixer,
 	hasLoaded = false,
 	clicked = false;
 
-initialize();
-animate();
-
-function initialize() {
+const initialize = () => {
 	scene = new THREE.Scene();
 
 	// let sceneLight = new THREE.PointLight(0xffffff, 0.5, 100);
@@ -36,7 +32,6 @@ function initialize() {
 	});
 	renderer.setClearColor(new THREE.Color("lightgrey"), 0);
 	renderer.setSize(window.innerWidth, window.innerHeight);
-	// renderer.setSize(640, 480);
 	renderer.domElement.style.position = "absolute";
 	renderer.domElement.style.top = "0px";
 	renderer.domElement.style.left = "0px";
@@ -52,26 +47,9 @@ function initialize() {
 
 	arToolkitSource = new THREEx.ArToolkitSource({
 		sourceType: "webcam",
-		sourceWidth: window.innerWidth > window.innerHeight ? 1280 : 960,
-		sourceHeight: window.innerWidth > window.innerHeight ? 960 : 1280,
-
-		// displayWidth: window.innerWidth > window.innerHeight ? 1280 : 960,
-		// displayHeight: window.innerWidth > window.innerHeight ? 960 : 1280,
 	});
 
-	arToolkitSource.init(function onReady() {
-		// window.arToolkitSource = arToolkitSource;
-		setTimeout(() => {
-			onResize();
-		}, 1000);
-	});
-
-	// handle resize event
-	window.addEventListener("resize", function () {
-		onResize();
-	});
-
-	function onResize() {
+	const onResize = () => {
 		arToolkitSource.onResizeElement();
 		arToolkitSource.copyElementSizeTo(renderer.domElement);
 		if (arToolkitContext.arController !== null) {
@@ -79,7 +57,17 @@ function initialize() {
 				window.arToolkitContext.arController.canvas
 			);
 		}
-	}
+	};
+
+	arToolkitSource.init(() => {
+		onResize();
+	});
+
+	// handle resize event
+	window.addEventListener("resize", () => {
+		onResize();
+	});
+
 	////////////////////////////////////////////////////////////
 	// setup arToolkitContext
 	////////////////////////////////////////////////////////////
@@ -87,13 +75,9 @@ function initialize() {
 	// create atToolkitContext
 	arToolkitContext = new THREEx.ArToolkitContext({
 		cameraParametersUrl: "js/camera_para.dat",
-		// maxDetectionRate: 60,
 		detectionMode: "mono_and_matrix",
-		// canvasWidth: 1280,
-		// canvasHeight: 960,
 	});
 
-	// copy projection matrix to camera when initialization complete
 	arToolkitContext.init(function onCompleted() {
 		camera.projectionMatrix.copy(arToolkitContext.getProjectionMatrix());
 		window.arToolkitContext = arToolkitContext;
@@ -112,40 +96,18 @@ function initialize() {
 		{
 			type: "pattern",
 			patternUrl: "markers/vhsoft_pattern.patt",
-			// barcodeValue: "6",
-			// smoothCount: "10",
-			// changeMatrixMode: "cameraTransformMatrix",
 		}
 	);
 
-	// let sceneGroup = new THREE.Group();
-	// scene.add(sceneGroup);
-
-	// let smoothedControls = new THREEx.ArSmoothedControls(sceneGroup, {
-	// 	lerpPosition: 0.6,
-	// 	lerpQuaternion: 0.6,
-	// 	lerpScale: 1,
-	// 	minUnvisibleDelay: 0.25, // when marker not visible, time to wait until model disappears
-	// });
-
-	// let geometry1 = new THREE.PlaneGeometry(1, 1, 4, 4);
-	// let material1 = new THREE.MeshBasicMaterial({
-	// 	color: 0x0000ff,
-	// 	opacity: 0.5,
-	// });
-	// mesh1 = new THREE.Mesh(geometry1, material1);
-	// mesh1.rotation.x = -Math.PI / 2;
-	// markerRoot1.add(mesh1);
-
-	function onProgress(xhr) {
+	const onProgress = (xhr) => {
 		console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-	}
+	};
 
-	function onError(xhr) {
+	const onError = (xhr) => {
 		console.error(xhr);
-	}
+	};
 
-	function loadModel(model) {
+	const loadModel = (model) => {
 		let loader = new GLTFLoader().setPath("models/");
 		loader.load(
 			model + ".glb",
@@ -164,38 +126,37 @@ function initialize() {
 
 				glbScene.position.y = -0.25;
 				glbScene.position.z = 0.5;
-				glbScene.position.x = -0.2;
+
 				glbScene.rotation.x = -Math.PI / 2;
-				glbScene.rotation.y = THREE.MathUtils.degToRad(30);
 				markerRoot1.add(glbScene);
 			},
 			onProgress,
 			onError
 		);
-	}
+	};
 
 	loadModel("doc_animated_light2");
-}
+};
 
-function update() {
+const update = () => {
 	// update artoolkit on every frame
 	if (arToolkitSource.ready !== false)
 		arToolkitContext.update(arToolkitSource.domElement);
 
 	if (hasLoaded && mixer !== undefined && clicked) mixer.update(deltaTime);
-}
+};
 
-function render() {
+const render = () => {
 	renderer.render(scene, camera);
-}
+};
 
-function animate() {
+const animate = () => {
 	requestAnimationFrame(animate);
 	deltaTime = clock.getDelta();
 	totalTime += deltaTime;
 	update();
 	render();
-}
+};
 
 animationBtn.addEventListener("click", () => {
 	if (hasLoaded && glbAnimations !== 0) {
@@ -214,3 +175,6 @@ animationBtn.addEventListener("click", () => {
 		alert("Model nie posiada animacji");
 	}
 });
+
+initialize();
+animate();
