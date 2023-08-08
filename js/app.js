4f2d5491d55d8883a573a13ae17d91632d7205ce
bvmbo <wiktorsz2001@wp.pl>
2023-08-08 18:35:48 +0200

    changed to cube model

--- js/app.js
+++ js/app.js
@@ -135,7 +135,7 @@ const initialize = () => {
 		});
 	};
 
-	let p1 = laodModel("models/doc_animated_light2.glb").then((result) => {
+	let p1 = laodModel("models/cube.glb").then((result) => {
 		model1 = result.scene;
 		modelAnimations = result.animations;
 	});
