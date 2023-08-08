64217c96bf26e6c44deaf68d763e6130b3e2e5fc
bvmbo <wiktorsz2001@wp.pl>
2023-08-08 18:41:41 +0200

    changed to celery model

--- js/app.js
+++ js/app.js
@@ -135,7 +135,7 @@ const initialize = () => {
 		});
 	};
 
-	let p1 = laodModel("models/cube.glb").then((result) => {
+	let p1 = laodModel("models/celery.glb").then((result) => {
 		model1 = result.scene;
 		modelAnimations = result.animations;
 	});
