98145e96af3e7ef87ad6e62a0d521b729b4399e0
bvmbo <wiktorsz2001@wp.pl>
2023-08-09 16:40:03 +0200

    changed to fixed doc model

--- js/app.js
+++ js/app.js
@@ -125,7 +125,7 @@ const initialize = () => {
 		});
 	};
 
-	let p1 = laodModel("models/doc_animated_light2.glb").then((result) => {
+	let p1 = laodModel("models/doc_new.glb").then((result) => {
 		model1 = result.scene;
 		modelAnimations = result.animations;
 		modelArray[0] = result;
